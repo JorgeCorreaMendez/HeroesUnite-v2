a86038e579bab66f7596b8adfe806c22d4d7f4d4
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2021-11-23 13:37:35 +0000

    Se han creado metodos para construir dos url una con todos los superheroes, y otra que le pases un id para buscar uno en especifico.

--- app/src/main/java/com/yogusoft/heroesunite/utils/NewtworkUtils.java
+++ app/src/main/java/com/yogusoft/heroesunite/utils/NewtworkUtils.java
@@ -12,9 +12,54 @@ import java.util.Scanner;
 
 public class NewtworkUtils {
     final static String SUPERHERO_BASE_URL = "https://akabab.github.io/superhero-api/api";
-    final static String ALL_SUPERHERO_URL = "/all.json";
+    final static String ALL_SUPERHEROES_URL = "/all.json";
 
     static int id;
-    final static String SEARCH_SUPERHERO_URS = "/id/" + id + ".json";
+    final static String SEARCH_SUPERHEROE_URS = "/id/" + id + ".json";
 
+    public static URL bluidUrlAllSuperHeroes(){
+        Uri builtUri = Uri.parse(SUPERHERO_BASE_URL + ALL_SUPERHEROES_URL).buildUpon().build();
+        URL url = null;
+
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            Log.e("NetworkUtils", "Error, en la generación de la url");
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static URL bluidUrlSearchSuperHeroe(int heroeId){
+        id = heroeId;
+        Uri builtUri = Uri.parse(SUPERHERO_BASE_URL + SEARCH_SUPERHEROE_URS).buildUpon().build();
+
+        URL url = null;
+
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            Log.e("NetworkUtils", "Error, en la generación de la url");
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        InputStream in = urlConnection.getInputStream();
+
+        Scanner read = new Scanner(in);
+        read.useDelimiter("\\A");
+
+        try {
+            if (read.hasNext()) return read.next();
+            else return null;
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
